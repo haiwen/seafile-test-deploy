e8e026bfa04dad78c720380942456d1fc9dd344a
lins05 <linshuai2012@gmail.com>
2014-08-19 11:32:22 +0800

    added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+./build.sh
+
+CCNET_CONF_DIR=/tmp/ccnet
+SEAFILE_CONF_DIR=/tmp/seafile-data
+
+######################
+# Initialize ccnet/seafile configuration
+######################
+ccnet-init -c ${CCNET_CONF_DIR} --name testserver --port 10001 --host 127.0.0.1
+seaf-server-init -d ${SEAFILE_CONF_DIR}
+
+######################
+# Stat ccnet/seafile
+######################
+ccnet-server -c ${CCNET_CONF_DIR}
+# wait for ccnet server
+sleep 3
+seaf-server -c ${CCNET_CONF_DIR} -d ${SEAFILE_CONF_DIR}
+fileserver -c ${CCNET_CONF_DIR} -d ${SEAFILE_CONF_DIR}
