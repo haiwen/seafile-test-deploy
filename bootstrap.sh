2c3edd11594454e88c3d069235a44ebe6cbf7827
陈钦亮 <helloworld.c@outlook.com>
2019-03-30 19:23:44 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,12 +14,12 @@ SEAFILE_CONF_DIR=/tmp/seafile-data
 ccnet-init -c ${CCNET_CONF_DIR} --name testserver --port 10001 --host 127.0.0.1
 seaf-server-init -d ${SEAFILE_CONF_DIR}
 
-cd ${CCNET_CONF_DIR} && cat > ccnet.conf <<EOF
+cd ${CCNET_CONF_DIR} && cat >> ccnet.conf <<EOF
 [Database]
 CREATE_TABLES=true
 EOF
 
-cd ${SEAFILE_CONF_DIR} && cat > seafile.conf <<EOF
+cd ${SEAFILE_CONF_DIR} && cat >> seafile.conf <<EOF
 [database]
 create_tables=true
 EOF
