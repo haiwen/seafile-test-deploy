42371e3b0881bef9d618eac7c1e821183ab70ff5
lian <imwhatiam123@gmail.com>
2024-06-25 09:47:11 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,29 @@ CCNET_CONF_DIR=/tmp/ccnet
 SEAFILE_CONF_DIR=/tmp/seafile-data
 LOG_DIR=/tmp/logs
 
+# -------------------------------------------
+# MariaDB
+# -------------------------------------------
+# https://mariadb.com/kb/en/mariadb-package-repository-setup-and-usage/
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-10.6"
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mariadb-server-10.6
+service mysql start
+
+SQLROOTPW=root
+
+mysqladmin -u root password $SQLROOTPW
+
+cat > /root/.my.cnf <<EOF
+[client]
+user=root
+password=$SQLROOTPW
+EOF
+
+chmod 600 /root/.my.cnf
+
+mysqladmin -uroot -proot create seafile
+mysqladmin -uroot -proot create ccnet
+
 ######################
 # Initialize ccnet/seafile configuration
 ######################
@@ -19,13 +42,28 @@ cd ${CCNET_CONF_DIR} && cat >> ccnet.conf <<EOF
 SERVICE_URL = http://127.0.0.1:8000
 
 [Database]
+ENGINE = mysql
+HOST = 127.0.0.1
+PORT = 3306
+USER = root
+PASSWD = root
+DB = ccnet
+CONNECTION_CHARSET = utf8mb4
 CREATE_TABLES=true
 EOF
 
 mkdir ${SEAFILE_CONF_DIR}
 cd ${SEAFILE_CONF_DIR} && cat >> seafile.conf <<EOF
 [database]
+type = mysql
+host = 127.0.0.1
+port = 3306
+user = root
+password = root
+db_name = seafile
+connection_charset = utf8mb4
 create_tables=true
+
 EOF
 
 ######################
